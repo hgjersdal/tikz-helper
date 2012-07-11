d5f47b6dae0c4f3c9b09364d9b42f32cb975b1ce
haavagj <hgjersdal@gmail.com>
2012-07-11 03:14:27 +0200

    Added example files. More cleaning up of function names

--- tikz-helper.lisp
+++ tikz-helper.lisp
@@ -71,8 +71,7 @@
 	 (format ,stream-name "\\end{tikzpicture}~%")
 	 (format ,stream-name "%%% Local Variables: ~%%%% mode: latex ~%%%% TeX-master: \"master\" ~%%%% End:~%~%")))))
 
-
-(defmacro tikz-clip ((plottingarea) &body body)
+(defmacro clip ((plottingarea) &body body)
   `(progn
      (format (ostream ,plottingarea) "\\begin{scope}~%")
      (format (ostream ,plottingarea) "\\clip (0,0) rectangle (~a,~a);~%" (width ,plottingarea) (height ,plottingarea))
@@ -85,30 +84,35 @@
       (setf my-list (append my-list (list (+ min (* n stepsize))))))
     my-list))
 
-(defun draw-tikz-line (plottingarea x-from y-from x-to y-to style)
+(defun draw-line (plottingarea x-from y-from x-to y-to style)
   (format (ostream plottingarea) "\\draw[~a] (~f,~f) -- (~f,~f);~%" style x-from y-from x-to y-to))
-(defun draw-tikz-node (plottingarea x y text style)
+(defun draw-node (plottingarea x y text style)
   (format (ostream plottingarea) "\\node[~a] at (~f,~f) {~a};~%" style x y text))
-(defun draw-tikz-vert-2pt (plottingarea x y style)
+(defun draw-vert-2pt (plottingarea x y style)
   (format (ostream plottingarea) "\\draw[~a] (~f,~fcm+1pt) -- (~f,~fcm - 1pt); ~%" style x y x y))
-(defun draw-tikz-circle (plottingarea x y style)
+(defun draw-circle (plottingarea x y style)
   (format (ostream plottingarea) "\\draw[~a] (~f,~f) circle(1pt); ~%" style x y))
+(defun draw-rectangle (plottingarea x-from y-from x-to y-to style)
+  (format (ostream plottingarea) "\\draw[~a] (~f,~f) rectangle (~f,~f);~%" style x-from y-from x-to y-to))
 
-(defun draw-tikz-profilepoint (plottingarea x y y-error style)
+(defun draw-profilepoint (plottingarea x y y-error style)
   (let ((yy (apply (make-transformation-y plottingarea) (list y)))
 	(yy-error (apply (make-vector-transform (height plottingarea) (plot-y-min plottingarea)
 						(plot-y-max plottingarea)) (list y-error))))
-    (draw-tikz-line plottingarea x (- yy yy-error) x (+ yy yy-error) style)
-    (draw-tikz-circle plottingarea x yy style)
+    (draw-line plottingarea x (- yy yy-error) x (+ yy yy-error) style)
+    (draw-circle plottingarea x yy style)
     (format (ostream plottingarea) "\\draw[~a] (~fcm -2pt,~f) -- (~fcm + 2pt,~f);~%" style x (- yy yy-error) x (- yy yy-error))
     (format (ostream plottingarea) "\\draw[~a] (~fcm -2pt,~f) -- (~fcm + 2pt,~f);~%" style x (+ yy yy-error) x (+ yy yy-error))))
 
+
+(defun make-histogram (min bin-size data)
+  (list :min min :bin-size bin-size :data data))
 
 (defun draw-histogram-top (tikz histo style)
   (let* ((data (map 'vector (make-transformation-y tikz) (getf histo :data)))
 	 (x-pos (map 'vector (make-transformation-x tikz)
 		     (make-range (getf histo :min) (getf histo :bin-size) (length data)))))
-    (draw-tikz-line tikz  (aref x-pos 0) (aref data 0) (aref x-pos 1) (aref data 0) style)
+    (draw-line tikz  (aref x-pos 0) (aref data 0) (aref x-pos 1) (aref data 0) style)
     (dotimes (n (- (length data) 1))
       (format (ostream tikz) "\\draw[~a] (~f,~f) -- (~f,~f) -- (~f,~f);~%"
 	      style
@@ -128,15 +132,27 @@
 	      (aref x-pos (+ n 1)) (aref data n)
 	      (aref x-pos (+ n 1)) 0))))
 
-(defun draw-tikz-graph (tikz x y line-style mark-style &optional (transformp t))
+(defun draw-graph (tikz x y line-style mark-style &optional (transformp t))
   (let* ((xx (if transformp (mapcar (make-transformation-x tikz) x) x))
 	 (yy (if transformp (mapcar (make-transformation-y tikz) y) y)))
     (unless (or (null x) (null y))
-      (draw-tikz-circle tikz (car xx) (car yy) mark-style))
+      (draw-circle tikz (car xx) (car yy) mark-style))
     (unless (or (null (cdr x)) (null (cdr y)))
-      (draw-tikz-line tikz (car xx) (car yy) (cadr xx) (cadr yy) line-style)
-      (draw-tikz-graph tikz (cdr xx) (cdr yy) line-style mark-style nil))))
+      (draw-line tikz (car xx) (car yy) (cadr xx) (cadr yy) line-style)
+      (draw-graph tikz (cdr xx) (cdr yy) line-style mark-style nil))))
 
 (defun draw-legend-point (tikz x y width line-style mark-style)
-  (draw-tikz-circle tikz x y mark-style)
-  (draw-tikz-line tikz (- x width) y (+ x width) y line-style))
+  (draw-circle tikz x y mark-style)
+  (draw-line tikz (- x (* 0.5 width)) y (+ x (* 0.5 width)) y line-style))
+
+(defun draw-legend-line (tikz x y width name line-style mark-style name-style)
+  (if (> (length mark-style) 0) (draw-circle tikz (+ (* 0.5 width) x) y mark-style))
+  (if (> (length line-style) 0) (draw-line tikz x y (+ x width) y line-style))
+  (draw-node tikz (+ x width) y name (concatenate 'string "right," name-style)))
+
+(defun draw-legend-rectangle (tikz x y width height name line-style fill-style name-style)
+  (if (> (length fill-style) 0) (draw-rectangle tikz x (- y (* 0.5 height))
+						(+ x width) (+ y (* 0.5 height)) fill-style))
+  (if (> (length line-style) 0) (draw-rectangle tikz x (- y (* 0.5 height))
+						(+ x width) (+ y (* 0.5 height)) line-style))
+  (draw-node tikz (+ x width) y name (concatenate 'string "right," name-style)))
