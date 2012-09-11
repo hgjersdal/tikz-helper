dd219a2bf92b7b42a93a4969c5d698eb360a1eef
haavagj <hgjersdal@gmail.com>
2012-09-11 23:58:57 +0200

    Updfates to README and added comments to spline.lisp

--- spline.lisp
+++ spline.lisp
@@ -8,9 +8,11 @@ by John Stockie
 (in-package :tikz-helper)
 
 (defun get-h (x)
+  "Get the n-1 long vector h"
   (map 'vector (lambda (xi xi+1) (- xi+1 xi)) x (subseq x 1)))
 
 (defun fill-matrix (h n matrix)
+  "Fill matrix describing the linear equations from row 1 to n-1. Same for not-a-knot and natural spline"
   (loop 
      for i from 1 below (- n 1)
      do
@@ -20,6 +22,7 @@ by John Stockie
   matrix)
 
 (defun make-not-a-know-matrix (h n)
+  "Make a matrix describing the not a knot system of equations"
   (let ((matrix (lla:make-matrix 'lla:dense n n :initial-element 0.0d0 :element-type 'double-float)))
     ;;top row
     (setf (lla:mref matrix 0 0) -1.0d0)
@@ -32,29 +35,29 @@ by John Stockie
     (fill-matrix h n matrix)))
 
 (defun make-natural-spline-matrix (h n)
+  "Make a matrix describing the natural spline system of equations"
   (let ((matrix (lla:make-matrix 'lla:dense n n :initial-element 0.0d0 :element-type 'double-float)))
     (setf (lla:mref matrix 0 0) 1.0d0)
     (setf (lla:mref matrix (- n 1) (- n 1)) 1.0d0)
     (fill-matrix h n matrix)))
 
 (defun make-right-hand-side (y h n)
+  "Right hand side for the system of spline equations."
   (let ((rhs (lla:make-matrix 'lla:dense n 1 :element-type 'double-float :initial-element 0.0d0)))
     (setf (lla:mref rhs 0 0) 0.0d0)
     (setf (lla:mref rhs (- n 1) 0) 0.0d0)
-    (loop 
-       for i from 1 below (- n 1)
-       do
+    (loop for i from 1 below (- n 1) do
 	 (setf (lla:mref rhs i 0) (* 6.0d0 (- (/ (- (aref y (+ i 1)) (aref y i)) (aref h i))
-					  (/ (- (aref y i) (aref y (- i 1))) (aref h (- i 1)))))))
+					      (/ (- (aref y i) (aref y (- i 1))) (aref h (- i 1)))))))
     rhs))
 
 (defun get-coeffs (h y m n)
+  "Coefficients used for calculating the spline segments"
   (let ((a (make-array n :element-type 'double-float :initial-element 0.0d0))
 	(b (make-array n :element-type 'double-float :initial-element 0.0d0))
 	(c (make-array n :element-type 'double-float :initial-element 0.0d0))
 	(d (make-array n :element-type 'double-float :initial-element 0.0d0)))
-    (loop for i from 0 below (- n 1)
-	 do
+    (loop for i from 0 below (- n 1) do
 	 (setf (aref a i) (aref y i))
 	 (setf (aref b i) (+ (/ (- (aref y (+ i 1)) (aref y i)) (aref h i))
 			     (- (/ (* (aref h i) (lla:mref m i 0)) 2.0d0))
@@ -64,6 +67,7 @@ by John Stockie
     (values a b c d)))
 
 (defun select-region (x xx)
+  "Figure out which spline segment to draw."
   (loop
      for i from 1 below (length x)
      when (<= xx (aref x i)) do
@@ -71,13 +75,13 @@ by John Stockie
      finally (return (- (length x) 2))))
   
 (defun get-spline-fun (x y &optional (natural nil))
+  "Returns the spline function fitted to x and y. If natural is T, get a natural cubic spline. If natural is NIL, a cubic spline with not-a-knot."
   (let* ((h (get-h x))
 	 (ms (lla:solve (if natural 
 			    (make-natural-spline-matrix h (length x))
 			    (make-not-a-know-matrix h (length x))) 
 			(make-right-hand-side y h (length x)))))
     (multiple-value-bind (a b c d) (get-coeffs h y ms (length x))
-      (format t "a:~a~%b:~a~%c:~a~%d:~a~%" a b c d)
       (lambda (xx)
 	(let* ((index (select-region x xx))
 	       (x-diff (- xx (aref x index))))
@@ -85,4 +89,3 @@ by John Stockie
 	     (* (aref b index) x-diff)
 	     (* (aref c index) x-diff x-diff)
 	     (* (aref d index) x-diff x-diff x-diff)))))))
-	     
