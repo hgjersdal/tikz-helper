6d0117dc399344e54e0a9519d199cc008a2c07a2
haavagj <hgjersdal@gmail.com>
2012-11-03 04:12:38 +0100

    Added axis and histo2d files.

---
+++ axis.lisp
@@ -0,0 +1,147 @@
+(in-package :tikz-helper)
+
+(defun range->steps (range step-min step-max &optional (power 0) (nth 0))
+  "Figure out the number of, and distance between tick marks."
+  (when (> nth 15) (error "Auto axis failed."))
+  (labels ((nsteps (step) (/ range (* step (expt 10 power))))
+	   (result (step) (values (* step (expt 10 power)) power (floor (nsteps step))))
+	   (withinp (step) (and (<= (nsteps step) step-max) (>= (nsteps step) step-min))))
+    (cond
+      ((>= (nsteps 10.0) step-max) (range->steps range step-min step-max (1+ power) (incf nth)))
+      ((<= (nsteps 1.0) step-min) (range->steps range step-min step-max (1- power) (incf nth)))
+      ((withinp 1.0) (result 1.0))
+      ((withinp 2.0) (result 2.0))
+      ((withinp 5.0) (result 5.0))
+      ((withinp 10.0) (result 10.0)))))
+
+(defun auto-ticks (min max step-min step-max)
+  "Generate a list of tick mark positions."
+  (multiple-value-bind (step power nsteps) (range->steps (- max min) step-min step-max)
+    (declare (ignorable power))
+    (let ((min-tick (* step (floor min step))))
+      (remove-if (lambda (x) (or (< x min) (> x max)))
+         (make-range min-tick step (* 2 nsteps))))))
+
+(defun auto-ticks-x (plottingarea x-list step-min step-max)
+  (if (listp x-list) x-list 
+      (auto-ticks (plot-x-min plottingarea) (plot-x-max plottingarea)  
+		  step-min step-max)))      
+
+(defun auto-ticks-y (plottingarea y-list step-min step-max)
+  (if (listp y-list) y-list 
+      (auto-ticks (plot-y-min plottingarea) (plot-y-max plottingarea)
+		  step-min step-max)))
+
+(defun draw-axis-rectangle (plottingarea 
+			    &key (fill nil) (rectangle-style "thick,black,fill=white")
+			      (precision-x 1) (precision-y 1)
+			      (x-ticks-max 10) (x-ticks-min 4) (y-ticks-max 10) (y-ticks-min 4)
+			      (x-list t) (y-list t) (tick-style ""))
+  "Draw a rectanle around the plottingarea, and add ticks. Keywords mean:
+fill: Fill the rectangle with fill color. Helpful if the plot is overlaying another plot.
+precision-x amd -y: Number of digits after '.'. If 0 all the tick mark names will be floored.
+x- of y-ticks-max or -min: The minimum or maximum number of tick marks in the x or y direction. With bad numbers, auto-tick will fail.
+x- or y- list: List of tick marks. If nil, no ticks are drawn. If it is a list, one tick mark per value in list. If T, ticks are automatically generated."
+  (scope (plottingarea rectangle-style)
+    (make-rectangle-path plottingarea
+			 (plot-x-min plottingarea) (plot-y-min plottingarea)
+			 (plot-x-max plottingarea) (plot-y-max plottingarea))
+    (path-stroke plottingarea t fill))
+  (draw-axis-ticks-x plottingarea (auto-ticks-x plottingarea x-list x-ticks-min x-ticks-max)
+		     :style tick-style :precision precision-x)
+  (draw-axis-ticks-y plottingarea (auto-ticks-y plottingarea y-list y-ticks-min y-ticks-max)
+		     :style tick-style :precision precision-y))
+
+(defun draw-axis-cross (plottingarea
+			&key (line-style "thick,black,fill=white")
+			  (x-ticks-max 10) (x-ticks-min 4) (y-ticks-max 10) (y-ticks-min 4)
+			  (precision-x 1) (precision-y 1)
+			  (x-list t) (y-list t) (tick-style ""))
+  "Draw axes that cross at the origin.
+Keywords mean:
+precision-x amd -y: Number of digits after '.'. If 0 all the tick mark names will be floored.
+x- of y-ticks-max or -min: The minimum or maximum number of tick marks in the x or y direction. With bad numbers, auto-tick will fail.
+x- or y- list: List of tick marks. If nil, no ticks are drawn. If it is a list, one tick mark per value in list. If T, ticks are automatically generated."
+  (scope (plottingarea line-style)
+    (path-move-to plottingarea (plot-x-min plottingarea) 0)
+    (path-line-to plottingarea (plot-x-max plottingarea) 0)
+    (path-move-to plottingarea 0 (plot-y-min plottingarea))
+    (path-line-to plottingarea 0 (plot-y-max plottingarea))
+    (path-stroke plottingarea))
+  (draw-axis-ticks-x plottingarea (remove 0.0 (auto-ticks-x plottingarea x-list  
+							    x-ticks-min x-ticks-max))
+		     :style tick-style
+		     :y-shift (format nil "~acm" (apply-transform-y plottingarea 0.0))
+		     :precision precision-x)
+  (draw-axis-ticks-y plottingarea (remove 0.0 (auto-ticks-y plottingarea y-list 
+							    y-ticks-min y-ticks-max))
+		     :style tick-style 
+		     :x-shift (format nil "~acm" (apply-transform-x plottingarea 0.0))
+		     :precision precision-y))
+
+(defun draw-axis-popped-out (plottingarea
+			&key (line-style "thick,black,fill=white")
+			  (precision-x 1) (precision-y 1)
+			  (x-ticks-max 10) (x-ticks-min 4) (y-ticks-max 10) (y-ticks-min 4)
+			  (x-list t) (y-list t) (x-shift "-0.15cm") (y-shift "-0.15cm")
+			  (tick-style ""))
+  "Draw axes that are shifted from plot-x-min and plot-y-min.
+Keywords mean:
+y- or x-shift: How far the axis should be shifted from plot-min.
+precision-x amd -y: Number of digits after '.'. If 0 all the tick mark names will be floored.
+x- of y-ticks-max or -min: The minimum or maximum number of tick marks in the x or y direction. With bad numbers, auto-tick will fail.
+x- or y- list: List of tick marks. If nil, no ticks are drawn. If it is a list, one tick mark per value in list. If T, ticks are automatically generated."
+  (scope (plottingarea line-style)
+    (path-move-to-mixed plottingarea (plot-x-min plottingarea) 0 (plot-y-min plottingarea) y-shift)
+    (path-line-to-mixed plottingarea (plot-x-max plottingarea) 0 (plot-y-min plottingarea) y-shift)
+    (path-move-to-mixed plottingarea (plot-x-min plottingarea) x-shift (plot-y-min plottingarea) 0)
+    (path-line-to-mixed plottingarea (plot-x-min plottingarea) x-shift (plot-y-max plottingarea) 0)
+    (path-stroke plottingarea))
+  (draw-axis-ticks-x plottingarea (auto-ticks-x plottingarea x-list 
+						x-ticks-min x-ticks-max)
+		     :style tick-style :y-shift y-shift :stop 0
+		     :precision precision-x)
+  (draw-axis-ticks-y plottingarea (auto-ticks-y plottingarea y-list 
+						y-ticks-min y-ticks-max)
+		     :style tick-style :x-shift x-shift :stop 0
+		     :precision precision-y))
+
+(defun draw-axis-left-bottom (plottingarea
+			      &key (line-style "thick,black,fill=white")
+				(x-ticks-max 10) (x-ticks-min 4) (y-ticks-max 10) (y-ticks-min 4)
+				(precision-x 1) (precision-y 1)
+				(x-list t) (y-list t) (tick-style ""))
+  "Draw axes along the left and bottom side of the figure.
+Keywords mean:
+precision-x amd -y: Number of digits after '.'. If 0 all the tick mark names will be floored.
+x- of y-ticks-max or -min: The minimum or maximum number of tick marks in the x or y direction. With bad numbers, auto-tick will fail.
+x- or y- list: List of tick marks. If nil, no ticks are drawn. If it is a list, one tick mark per value in list. If T, ticks are automatically generated."
+  (draw-line plottingarea (plot-x-min plottingarea) (plot-y-min plottingarea) 
+	     (plot-x-max plottingarea) (plot-y-min plottingarea) line-style)
+  (draw-line plottingarea (plot-x-min plottingarea) (plot-y-min plottingarea)
+	     (plot-x-min plottingarea) (plot-y-max plottingarea) line-style)
+  (draw-axis-ticks-x plottingarea (auto-ticks-x plottingarea x-list 
+						x-ticks-min x-ticks-max)
+		     :style tick-style :precision precision-x)
+  (draw-axis-ticks-y plottingarea (auto-ticks-y plottingarea y-list
+						y-ticks-min y-ticks-max)
+		     :style tick-style :precision precision-y))
+
+(defun draw-grid-lines (plottingarea
+			&key (line-style "thin,gray")
+			  (x-ticks-max 10) (x-ticks-min 4) (y-ticks-max 10) (y-ticks-min 4)
+			  (x-list t) (y-list t))
+  "Draw grid lines.
+Keywords mean:
+x- of y-ticks-max or -min: The minimum or maximum number of tick marks in the x or y direction. With bad numbers, auto-tick will fail.
+x- or y- list: List of tick marks. If nil, no ticks are drawn. If it is a list, one tick mark per value in list. If T, ticks are automatically generated."
+  (flet ((x-lines (y-val) 
+	   (path-move-to plottingarea (plot-x-min plottingarea) y-val)
+	   (path-line-to plottingarea (plot-x-max plottingarea) y-val))
+	 (y-lines (x-val) 
+	   (path-move-to plottingarea x-val (plot-y-min plottingarea))
+	   (path-line-to plottingarea x-val (plot-y-max plottingarea))))
+    (scope (plottingarea line-style)
+      (mapcar #'y-lines (auto-ticks-x plottingarea x-list x-ticks-min x-ticks-max))
+      (mapcar #'x-lines (auto-ticks-y plottingarea y-list y-ticks-min y-ticks-max))
+      (path-stroke plottingarea))))
---
+++ histo2d.lisp
@@ -0,0 +1,177 @@
+(in-package :tikz-helper)
+
+(defun make-histogram2d (x-min x-bin-size x-nbin y-min y-bin-size y-nbin)
+  "Alloacte a histogram"
+  (let ((data (make-array (list x-nbin y-nbin) :element-type 'double-float)))
+    (list :x-min x-min :x-bin-size x-bin-size :x-nbin x-nbin :y-min y-min :y-bin-size y-bin-size :y-nbin y-nbin :data data)))
+
+(defun histo2d-incf (histo x y &optional (val 1.0d0))
+  "Increment the histogram bin at x y"
+  (let ((x-bin (floor (- x  (getf histo :x-min)) (getf histo :x-bin-size)))
+	(y-bin (floor (- y (getf histo :y-min)) (getf histo :y-bin-size))))
+    (when (and (>= x-bin 0) (>= y-bin 0)
+	       (< x-bin (getf histo :x-nbin)) (< y-bin (getf histo :y-nbin)))
+      (incf (aref (getf histo :data) x-bin y-bin) val))))
+
+(defun histo2d-get-max (histo)
+  "Get the bin with the largest content"
+  (max (reduce #'max (make-array (* (getf histo :x-nbin)
+				    (getf histo :y-nbin))
+				 :element-type 'double-float
+				 :displaced-to (getf histo :data)))))
+
+(defun make-color-combo (z-min z-max val cols)
+  ;; "Make a color somehwere between first and last in color, with gradients in between"
+  (let* ((ncol (length cols))
+	 (nbins (- ncol 1))
+	 (z% (/ (* 100 nbins (- val z-min)) (- z-max z-min))))
+    (multiple-value-bind (zbin col) (floor z% 100)
+      (cond ((< zbin 0) (first cols))
+	    ((>= zbin nbins) (elt cols (- ncol 1)))
+	    (t (format nil "~a!~a!~a" (elt cols (1+ zbin)) (floor col)
+		       (elt cols zbin)))))))
+
+(defun draw-histo2d-rectangles (plottingarea histo z-min z-max
+				&optional (cols (list "blue" "green" "yellow" "red")))
+  "Draw a rectangle for each bin. Colors go from cold to hot"
+  (let ((x-poses (make-range (getf histo :x-min) (getf histo :x-bin-size) (getf histo :x-nbin)))
+	(y-poses (make-range (getf histo :y-min) (getf histo :y-bin-size) (getf histo :y-nbin)))
+	(x-bins (make-range 0 1 (getf histo :x-nbin)))
+	(y-bins (make-range 0 1 (getf histo :y-nbin))))
+    (labels ((draw-bin (x-pos y-pos x-bin y-bin)
+	       (scope (plottingarea (make-color-combo z-min z-max (aref (getf histo :data) x-bin y-bin) 
+						      cols))
+		 (make-rectangle-path plottingarea x-pos y-pos
+				      (+ x-pos (getf histo :x-bin-size))
+				      (+ y-pos (getf histo :y-bin-size)))
+		 (path-stroke plottingarea t t nil)))
+	     (draw-row (y-bin y-pos)
+	       (map nil (lambda (x-bin x-pos) (draw-bin x-pos y-pos x-bin y-bin)) x-bins x-poses)))
+      (map nil #'draw-row y-bins y-poses))))
+
+(defun check-neighbour (x y val data cmap)
+  "See if there are borders around the cell. Border means the cell is above val, the neighbour is below."
+  (flet ((isbelow (x y)
+	   (let ((dim (array-dimensions cmap)))
+	     (cond ((or (< x 0) (< y 0)) t)
+		   ((or (>= x (first dim)) (>= y (second dim))) t)
+		   (t (< (aref data x y) val))))))
+    (when (>= (aref data x y) val)
+      ;;Vector is left up right below
+      (let ((nb (vector (isbelow (- x 1) y) (isbelow x (+ y 1))
+			(isbelow (+ x 1) y) (isbelow x (- y 1)))))
+	(when (reduce (lambda (x y) (or x y)) nb)
+	  (setf (aref cmap x y) nb))))))
+
+(defun make-contour-cells (histo cmap z-val)
+  "Count borders for all the cells in histo"
+  (let ((data (getf histo :data))
+	(xbins (make-range 0 1 (getf histo :x-nbin)))
+	(ybins (make-range 0 1 (getf histo :y-nbin))))
+    (mapc (lambda (x) (mapc (lambda (y) (check-neighbour x y z-val data cmap)) ybins)) xbins)))
+
+(defun dir-to-num (dir)
+  (case dir (:left 0) (:up 1) (:right 2) (:down 3)))
+
+(defun num-to-dir (num)
+  (elt (list :left :up :right :down) num))
+
+(defun oposite-dir (dir)
+  (let ((num (dir-to-num dir)))
+    (num-to-dir (if (> num 1) (- num 2) (+ 2 num)))))
+
+(defun turn-left (dir)
+  (let ((num (dir-to-num dir)))
+    (num-to-dir (if (= num 0) 3 (- num 1)))))
+
+(defun turn-right (dir)
+  (let ((num (dir-to-num dir)))
+    (num-to-dir (if (= num 3) 0 (+ num 1)))))
+
+(defun turn-left-xy (dir xy)
+  (mapcar #'+ xy
+	  (case dir (:left (list -1 -1)) (:up (list -1 1))
+		(:right (list 1 1)) (:down (list 1 -1)))))
+
+(defun continue-xy (dir xy)
+  (mapcar #'+ xy
+	  (case dir (:left  (list -1 0)) (:up    (list 0 1))
+		(:right (list 1 0)) (:down  (list 0 -1)))))
+
+(defun borderp (xy borderdir cmap)
+  "Does cell xy have a border in direction borderdir?"
+  (let ((dim (array-dimensions cmap)))
+    (cond ((minusp (elt xy 0)) nil)
+	  ((minusp (elt xy 1)) nil)
+	  ((>= (elt xy 0) (elt dim 0)) nil)
+	  ((>= (elt xy 1) (elt dim 1)) nil)
+	  ((null (aref cmap (first xy) (second  xy))) nil)
+	  ((aref (aref cmap (first xy) (second  xy)) (dir-to-num borderdir)) t))))
+
+(defun border-correction (max-bin min-bin val)
+  "Linear interpolation"
+  (if (= max-bin min-bin) 0.0
+      (- (/ (- val min-bin) (- max-bin min-bin))  0.5d0)))
+
+(defun move-to-border (xy dir histo val)
+  "Which side of the cell should the contour line be on? Positions are corrected with a linear interpolation."
+  (flet ((get-correction (xy x-shift y-shift)
+	   (let ((dim (array-dimensions (getf histo :data))))
+	     (if (or (minusp (+ x-shift (first xy))) (minusp (+ y-shift (second xy)))
+		     (>= (+ x-shift (first xy)) (elt dim 0)) (>= (+ y-shift (second xy)) (elt dim 1))
+		     (<= val 0)) 
+		 0.0
+		 (border-correction (aref (getf histo :data) (first xy) (second xy))
+				    (aref (getf histo :data) (+ x-shift (first xy)) (+ y-shift (second xy)))
+				    val)))))
+    (mapcar #'+ xy (case dir
+		     (:left (list (get-correction xy -1 0) 0.5))
+		     (:up (list 0.5 (- 1.0 (get-correction xy 0 1))))
+		     (:right (list (- 1.0 (get-correction xy 1 0)) 0.5))
+		     (:down (list 0.5 (get-correction xy 0 -1)))))))
+
+(defun add-contour-path-point (xy dir histo tikz val)
+  "Add a contour point to path,"
+  (let* ((xy (move-to-border xy dir histo val))
+	 (x (+ (getf histo :x-min) (* (elt xy 0) (getf histo :x-bin-size))))
+	 (y (+ (getf histo :y-min) (* (elt xy 1) (getf histo :y-bin-size)))))
+    (path-line-to tikz x y)))
+
+(defun continue-path (xy dir cmap histo tikz val)
+  "Stroll along path with border at left hand side."
+  (let ((borderdir (turn-left dir)))
+    (setf (aref (aref cmap (first xy) (second xy)) (dir-to-num borderdir)) nil)
+    (add-contour-path-point xy borderdir histo tikz val))
+  (cond ((borderp (turn-left-xy dir xy) (oposite-dir dir) cmap)
+	 (continue-path (turn-left-xy dir xy) (turn-left dir) cmap histo tikz val))
+	((borderp (continue-xy dir xy) (turn-left dir) cmap)
+	 (continue-path (continue-xy dir xy) dir cmap histo tikz val))
+	((borderp xy dir cmap)
+	 (continue-path xy (turn-right dir) cmap histo tikz val))))
+
+(defun start-contour-line (x y cmap histo tikz val)
+  "Start the contour line, if cell has borders."
+  (let ((cell (aref cmap x y)))
+    (unless (or (null cell) (reduce (lambda (x y) (and (not x) (not y))) cell))
+      (let* ((dir (num-to-dir 
+		   (cond ((aref cell 0) 0) ((aref cell 1) 1) ((aref cell 2) 2) ((aref cell 3) 3))))
+	     (xy (move-to-border (list x y) dir histo val))
+	     (xx (+ (getf histo :x-min) (* (first xy) (getf histo :x-bin-size))))
+	     (yy (+ (getf histo :y-min) (* (second xy) (getf histo :y-bin-size)))))
+	(path-move-to tikz xx yy)
+	(continue-path (list x y) (turn-right dir) cmap histo tikz val)
+	(path-close tikz)))))
+
+(defun draw-histo2d-contour (plottingarea histo z-min z-max nlines fillp
+			     &optional (cols (list "blue" "green" "yellow" "red")))
+  "Draw possibly filled contour lines."
+  (let ((cmap (make-array (list (getf histo :x-nbin) (getf histo :y-nbin)) :initial-element nil))
+	(data (getf histo :data))
+	(xbins (make-range 0 1 (getf histo :x-nbin)))
+	(ybins (make-range 0 1 (getf histo :y-nbin))))
+    (dotimes (i nlines)
+      (let ((height (+ z-min (* i (/ (- z-max z-min) nlines)))))
+	(mapc (lambda (x) (mapc (lambda (y) (check-neighbour x y height data cmap)) ybins)) xbins)
+	(scope (plottingarea (format nil "draw=black,fill=~a" (make-color-combo z-min z-max height cols)))
+	  (mapc (lambda (x) (mapc (lambda (y) (start-contour-line x y cmap histo plottingarea height)) ybins)) xbins)
+	  (path-stroke plottingarea t fillp))))))
